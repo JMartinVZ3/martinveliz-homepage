b6937def1df2f58e3033c189f52042b812c1a252
Martin Veliz <54328819+JMartinVZ3@users.noreply.github.com>
2022-01-25 16:00:00 -0500

    <feat>{added navbar and some styles}

---
+++ components/logo.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Text, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 50px;
+  line-height: 20px;
+  padding: 10px;
+
+  img {
+    transition: 200ms ease;
+  }
+`
+
+const Logo = () => {
+  const footPrintImg = `/images/martinvelizlogo${useColorModeValue('-dark', '')}.png`
+
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={footPrintImg} width={30} height={30} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily='M PLUS Rounded 1c", sans-serif'
+            fontWeight="bold"
+            ml={3}
+          >
+            Martin Veliz
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
+}
+
+export default Logo
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layout/main'
+import Fonts from '../components/fonts'
+import theme from '../lib/theme'
+import { AnimatePresence } from 'framer-motion'
+import { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps, router  }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Fonts />
+      <Layout router={router}>
+        <AnimatePresence exitBeforeEnter initial={true}>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Layout>
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
